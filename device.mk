7f4f3dbe8b50ebe5ecad13ade492c2c0f0428efb
Manuel José M <az.az.az.mjme@gmail.com>
2023-11-07 17:15:35 +0100

    alioth: Merge overlay-lineage into overlay
    Signed-off-by: Manuel José M <az.az.az.mjme@gmail.com>

--- device.mk
+++ device.mk
@@ -49,8 +49,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Device Marketname Overlays
 PRODUCT_PACKAGES += \
