e8bf97b62ab4ecbd1ec5555ce91da1ea742c0a0f
masemoel <az.az.az.mjme@gmail.com>
2024-04-28 15:58:22 +0200

    alioth: Merge overlay-lineage into overlay
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

--- device.mk
+++ device.mk
@@ -46,8 +46,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
